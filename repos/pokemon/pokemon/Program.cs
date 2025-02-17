7537adbff1cdb64af5240074adb0a1aafd42da4e
YilmazNL2005 <99069863@mydavinci.nl>
2025-02-17 09:44:31 +0100

    test

---
+++ repos/pokemon/pokemon/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using pokemon;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World!");
+string antwoord = Console.ReadLine();
+string name = "pikachu";
+int hp = 150;
+string type = "fire";
+Console.WriteLine(name);
+
+var pikachu1 = new pikachu(name, hp , type  );
+pikachu1.name = "charmander";
+pikachu1.hp = 30;
+pikachu1.type = "fire";
+
+
+var pokeball1 = new pokeball(type, );
+
+
+Console.WriteLine(pikachu1.name + " - " + pikachu1.hp);
+
+// Notities
+
+pikachu1.geeftles();
+
+bool running = true;
+while(running)
+{
+    Console.WriteLine("Wat is de naam van je pokemon?");
+    pikachu1.name = Console.ReadLine();
+    Console.WriteLine("Hoeveel hp heeft je pokemon?");
+    //pikachu1.hp = Console.ReadLine();
+    Console.WriteLine("Wat is het type van je pokemon?");
+    for (int x = 0; x < 10; x++)
+    {
+        pikachu1.geeftles();
+    }
+
+          
+}
---
+++ repos/pokemon/pokemon/pikachu.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokemon
+{
+    public class pikachu
+    {
+        public string name { get; set; }
+        public int hp { get; set; }
+        public string type { get; set; }
+        public pikachu(string name, int hp, string type)
+        {
+         this.name = name;
+         this.hp = hp;
+         this.type = type;
+        }
+
+
+        public void geeftles()
+        {
+            Console.WriteLine(this.name);
+            Console.WriteLine("Blah Blah Blah...");
+        }
+           
+    }
+
+    public class charmander
+    {
+        public string name { get; set; }
+        public int hp { get; set; }
+        public string type { get; set; }
+        public charmander(string name, int hp, string type)
+        {
+            this.name = name;
+            this.hp = hp;
+            this.type = type;
+        }
+
+        public void char_sound()
+        {
+            Console.WriteLine(this.name);
+            Console.WriteLine("Rawr Rawr Rawr...");
+        }
+
+    }
+
+    public class trainer
+    {
+        public string name { get; set; }
+        public trainer(string name)
+        {
+            this.name = name;
+        }
+        public List<pokeball> belt; 
+
+        public void trainer_sound()
+        {
+            Console.WriteLine(this.name);
+            Console.WriteLine("Rawr Rawr Rawr...");
+        }
+
+    }
+
+    public class pokeball
+    {
+        public charmander content { get; set; }
+        public pokeball(charmander pokemon)
+        {
+            this.content = pokemon;
+        }
+
+    }
+
+//    public class pokeball
+//{
+//    public string type { get; set; }
+//    public int level { get; set; }
+//    public pikachu
+//    public charmander(string name, int hp, string type)
+//    {
+//        this.name = name;
+//        this.hp = hp;
+//        this.type = type;
+//    }
+//}
